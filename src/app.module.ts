3cf501c823c8c8c84353067eebef72efd061b876
tadatakuho <tadatakuho@team-lab.com>
2022-03-05 13:07:40 +0900

    graphql, prisma導入

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { ApolloDriver } from '@nestjs/apollo';
+import { join } from 'path';
+import { UserResolver } from './resolver/user.resolver';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      debug: true,
+      playground: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UserResolver],
 })
 export class AppModule {}
